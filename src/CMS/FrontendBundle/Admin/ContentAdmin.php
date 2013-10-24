0cdf109a5c3df5cd08eef492deb082c4aa5d1400
Luciano Andrade <andrade.luciano@gmail.com>
2013-10-24 20:50:15 -0300

    Sonata Admin for Content.

---
+++ src/CMS/FrontendBundle/Admin/ContentAdmin.php
@@ -0,0 +1,40 @@
+<?php
+namespace CMS\FrontendBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class ContentAdmin extends Admin
+{
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('title', 'text', array('label' => 'Content Title'))
+            ->add('body')
+            ->add('featured')
+        ;
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('title')
+            ->add('body')
+            ->add('featured')
+        ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('title')
+            ->add('body')
+            ->add('featured')
+        ;
+    }
+}
