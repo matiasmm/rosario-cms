343f47a7e045d1dcbe9457492d4c14edba81e5b0
Matias Montenegro <matiasmmontenegro@gmail.com>
2013-10-24 21:56:17 -0300

    Changes in routing files

--- src/CMS/FrontendBundle/Controller/DefaultController.php
+++ src/CMS/FrontendBundle/Controller/DefaultController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name){
+    public function indexAction(){
         $contents = $this->getDoctrine()->getRepository('CMSFrontendBundle:Content')
         ->findAll();
 
