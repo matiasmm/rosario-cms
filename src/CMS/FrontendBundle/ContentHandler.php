4654b98121cad2ebd74b262f612e09870b375ebf
Matias Montenegro <matiasmmontenegro@gmail.com>
2013-11-28 19:54:45 -0300

    Installed grunt

---
+++ src/CMS/FrontendBundle/ContentHandler.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: matias
+ * Date: 14/11/13
+ * Time: 20:53
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace CMS\FrontendBundle;
+
+
+class ContentHandler {
+
+}
